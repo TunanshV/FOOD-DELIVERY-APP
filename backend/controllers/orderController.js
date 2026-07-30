9ba41671d36169148ebb984f4c03aea054aa5839
TunanshV <tunanshvatsa@gmail.com>
2026-07-30 10:46:13 +0530

    Successfully finished the project

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,7 +10,7 @@ const stripe= new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeOrder= async(req,res)=>{
 
-    const frontend_url= "http://localhost:5173";
+    const frontend_url= "http://localhost:5174";
 
 
     try {
@@ -96,4 +96,30 @@ const  userOrders= async (req,res)=>{
       
 }
 
-export {placeOrder,verifyOrder,userOrders}
+
+// Listing orders for the admin panel
+const listOrders= async(req,res)=>{
+     try {
+          const orders= await orderModel.find({});
+          res.json({success: true,data: orders})
+     } catch (error) {
+          console.log(error);
+          res.json({success: false,message:"Error"})
+     }
+}
+
+
+
+// api for updating order status
+const updateStatus= async(req,res)=>{
+     try {
+         await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+         res.json({success:true,message:"Status Updated"})
+     } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+     }
+      
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders,updateStatus}
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.js"
-import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js"
+import {  listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from "../controllers/orderController.js"
 
 
 
@@ -10,6 +10,8 @@ const orderRouter= express.Router();
 orderRouter.post("/place",authMiddleware,placeOrder);
 orderRouter.post("/verify",verifyOrder);
 orderRouter.post("/userorders",authMiddleware,userOrders);
+orderRouter.get("/list",listOrders);
+orderRouter.post("/status",updateStatus);
 
 
 export default orderRouter;
